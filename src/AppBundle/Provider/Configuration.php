137e26d09d6138ccb9b06c5413c47f0a3fd12483
jdeveloper <jdeveloper.inxenio@gmail.com>
2015-06-30 16:00:55 +0200

    If no configuration file is aviaible, we read the default.yml.dist

--- src/AppBundle/Provider/Configuration.php
+++ src/AppBundle/Provider/Configuration.php
@@ -9,20 +9,31 @@ class Configuration
     static function loadYml($request, $affiliate_dir)
     {
         $host = $request->getHost();
-        $yml = false;
-
-        $yml = @file_get_contents(__DIR__ ."/".$affiliate_dir.$host.'.yml');
-
-        if($yml === FALSE && !is_null($affiliate_dir)){
-        	$yml = @file_get_contents(__DIR__ . "/".$affiliate_dir. "/default.yml");
-        }
-
-        if($yml === FALSE){
-        	$yml = @file_get_contents(__DIR__ . '/afiliates/default.yml');
-        }
+        $yml = self::loadConfigYmlFile($affiliate_dir);
 
         $parameters = Yaml::parse($yml);
 
         return $parameters;
+    }
+
+    static function loadConfigYmlFile($host)
+    {
+        $candidateFiles = array(
+            __DIR__ . '/afiliates/'.$host.'.yml',
+            __DIR__ . '/afiliates/default.yml',
+            __DIR__ . '/afiliates/default.yml.dist'
+        );
+
+        $yml = false;
+
+        foreach($candidateFiles as $candidateFile){
+            $yml = @file_get_contents($candidateFile);
+
+            if($yml !== false){
+                return $yml;
+            }
+        }
+
+        return $yml;
     }
 }
