e77836b253dab2c51e340138b1fb0320c0079a60
ant4 <jdeveloper.inxeio@gmail.com>
2015-07-09 11:23:43 +0200

    Now the provider configuration is reades as follow:
    First we read de default config:
    
       if the app has a default.yml file we readit
       else we read the file specified in the affiliate_dir_path parameter dir
    
    Then we load if it exists the specific config file of the host to merge it with the default config, the config is read as follows:
    
       if the app has a host file we read it
       els we look it the file for the host is located in the dir specified by the affiliate_dir_path parameter

--- src/AppBundle/Provider/Configuration.php
+++ src/AppBundle/Provider/Configuration.php
@@ -9,31 +9,43 @@ class Configuration
     static function loadYml($request, $affiliate_dir)
     {
         $host = $request->getHost();
-        $yml = self::loadConfigYmlFile($affiliate_dir);
-
-        $parameters = Yaml::parse($yml);
-
-        return $parameters['parameters'];
+        return self::loadConfig($affiliate_dir, $host);
     }
 
-    static function loadConfigYmlFile($host)
+    static function loadConfig($affiliate_dir, $host)
     {
-        $candidateFiles = array(
-            __DIR__ . '/afiliates/'.$host.'.yml',
-            __DIR__ . '/afiliates/default.yml',
+        $configs = array_merge(
+            self::loadFistAviaibleYml(array($affiliate_dir.'default.yml', __DIR__ . '/afiliates/default.yml')),
+            self::loadFistAviaibleYml(array($affiliate_dir.$host.'.yml', __DIR__ . '/afiliates/'.$host.'.yml'))
         );
 
+        if(count($configs) == 0){
+            $candidateFiles = array(
+                $affiliate_dir.'default.yml',
+                __DIR__.'/afiliates/default.yml',
+                $affiliate_dir.$host.'.yml',
+                __DIR__.'/afiliates/'.$host.'.yml'
+            );
+
+            $message = sprintf("There are no configuration parameters for this host. Create one of the following files: %s",
+                implode(", ", $candidateFiles));
+
+            throw new \Exception($message);
+        }
+
+        return $configs;
+    }
+
+    static function loadFistAviaibleYml($candidateFiles)
+    {
         foreach($candidateFiles as $candidateFile){
             $yml = @file_get_contents($candidateFile);
 
             if($yml !== false){
-                return $yml;
+                return Yaml::parse($yml)['parameters'];
             }
         }
 
-        $message = sprintf("There are no configuration parameters for this host. Create one of the following files: %s",
-            implode(", ", $candidateFiles));
-
-        throw new \Exception($message);
+        return array();
     }
 }
