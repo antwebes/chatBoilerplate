10cf1189df78017ae5cea5e0ed67375ad86aca58
Chrysweel <festixeiro@gmail.com>
2016-11-16 19:45:32 +0100

    [COMPOSER] update vendors

--- src/Ant/Bundle/OfferBundle/Controller/OfferController.php
+++ src/Ant/Bundle/OfferBundle/Controller/OfferController.php
@@ -25,7 +25,7 @@ class OfferController extends Controller
 
         $user = $this->getUserOnline();
 
-        $entities = $em->getRepository('OfferBundle:Offer')->findAll();
+        $entities = $em->getRepository('OfferBundle:Offer')->finByCriteria(array('enabled'=> true));
 
         return $this->render('OfferBundle:Offer:index.html.twig', array(
             'entities' => $entities,
